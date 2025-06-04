95ccd3f02ced19132d1b82dc80b4862950f6010e
James Lewis <james@bionicnrg.com>
2025-06-03 22:29:59 -0700

    Update pwd generator example project
    updated this to use argparse

--- homelab_platform/python/pyProd/pwdGenerator_001.py
+++ homelab_platform/python/pyProd/pwdGenerator_001.py
@@ -3,29 +3,35 @@
     password generator
 .Description
     password generator
+    generates random password and will be handled from cmdline via py filename.py --arg --arg
 .Author
     James Lewis
 .Date
-    09/09/2024
+    03/12/2025
 """
 
 import random
+import argparse
 
-print('Welcome To Password Generator')
+def genPwd(number, length):
 
-chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*(),.'
+    print('Welcome To Password Generator')
 
-number = input('Amount of passwords to generate: ')
-number = int(number)
+    chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*(),.'
 
-length = input("Your Password length: ")
-length = int(length)
+    print("\nhere are your passwords: ")
 
-print("\nhere are your passwords: ")
+    for pwd in range(number):
+        passwords = ""
+        for c in range(length):
+            passwords += random.choice(chars)
+        print(passwords)
 
-for pwd in range(number):
-    passwords = ""
-    for c in range(length):
-        passwords += random.choice(chars)
-    print(passwords)
 
+cmdLine = argparse.ArgumentParser(description="Password Generator")
+cmdLine.add_argument("--number", type=int, default=5, help="amount of passwords to generate")
+cmdLine.add_argument("--length", type=int, default=8, help="length of each password")
+
+args = cmdLine.parse_args()
+
+genPwd(args.number, args.length)
